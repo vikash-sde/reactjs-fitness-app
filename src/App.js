49128baca67f99f58e700c6eab6c02d07470d190
Vikash Gupta <vikashg2512@gmail.com>
2022-04-03 17:27:19 +0530

    required packages setup

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Pages from './pages/Pages';
 
 function App() {
   return (
-    <div className="App">
+    <div >
       <Pages/>
     </div>
   );
--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,21 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
 
 const Popular = () => {
+  const [popular, setPopular] = useState([]);
+
   const getPopular = async () => {
     const api = await fetch(
       `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
     );
     const data = await api.json();
-    console.log(data);
+    setPopular(data.recipes);
+    console.log(data.recipes);
   };
 
   useEffect(() => {
-getPopular()
-  }, [])
-    console.log(process.env.REACT_APP_API_KEY);
-  
+    getPopular();
+  }, []);
 
-  return <div>Popular</div>;
+  return (
+    <div>
+      {popular.map((recipe) => {
+        return (
+          <div key={recipe.id}>
+            <p>{recipe.title}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
 
 export default Popular;
