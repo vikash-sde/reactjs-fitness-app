a538d78ac16d7fc533c4fcd459a9cca635752366
Vikash Gupta <vikashg2512@gmail.com>
2022-04-03 10:39:11 +0530

    api auth bugfixed

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 const Popular = () => {
   const getPopular = async () => {
     const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apikey=${process.env.REACT_APP_API_KEY}&number=9`
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
     );
     const data = await api.json();
     console.log(data);
